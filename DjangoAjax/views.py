7e5a0d742b53f4bb320885c4a2a68cf6c3941d8f
Chad.Cao <Chad.Cao@movit-tech.com>
2020-02-13 00:57:40 +0800

    优化代码： 1）人员根据外键获取岗位

--- DjangoAjax/views.py
+++ DjangoAjax/views.py
@@ -8,7 +8,7 @@ from django.http import JsonResponse
 from django.shortcuts import render
 
 # Create your views here.
-from DjangoAjax.models import User, Position, UserPosition, VALID
+from DjangoAjax.models import User, Position, UserPosition, VALID, INVALID
 
 # 路由跳转到首页
 from PublicMethod.common import model2json
@@ -31,7 +31,7 @@ def add(request):
         save_id = transaction.savepoint()
         try:
             # 添加人员
-            user_id = str(uuid.uuid4());
+            user_id = uuid.uuid4();
             User.objects.create(
                 id=user_id
                 , user_no=user_json['user_no']
@@ -40,8 +40,7 @@ def add(request):
                 , password=user_json['password']);
             # 添加人员岗位
             UserPosition.objects.create(
-                id=uuid.uuid4()
-                , user_id=user_id
+                user_id=user_id
                 , position_id=user_json['pos_id']);
             # 提交订单成功，显式的提交一次事务
             transaction.savepoint_commit(save_id)
@@ -57,7 +56,8 @@ def delItem(request, id):
     res = {"code": "200", "err_msg": "", "data": ""};
     try:
         item = User.objects.get(id=id);
-        item.delete();
+        item.status = INVALID;
+        item.save();
     except Exception as e:
         res["code"] = "500";
         res["err_msg"] = '删除用户失败';
@@ -92,7 +92,15 @@ def getItem(request):
         try:
             id = request.POST.get('id');
             item = User.objects.get(id=id);
+            # 获取岗位
+            position_id = '';
+            user_position_list = item.userposition_set;
+            if user_position_list is not None:
+                userposition = item.userposition_set.first();
+                if userposition is not None:
+                    position_id = userposition.position_id;
             item_json = model2json(item);
+            item_json["pos_id"] = position_id;
             res['data'] = item_json;
         except Exception as e:
             res["code"] = "500";
@@ -101,15 +109,23 @@ def getItem(request):
 
 
 # 根据用户编码获取对象
-def getItemByNo(request):
+def isUserNOExists(request):
     if request.method == 'POST':
         res = {"code": "200", "err_msg": "", "data": ""};
         try:
             user_no = request.POST.get('user_no');
-            userlist = User.objects.filter(user_no=user_no).values();
-            if (userlist.count() > 0):
-                item = list(userlist)[0];
-                res['data'] = item;
+            user_id = request.POST.get('user_id');
+            item = '';
+            if user_id == '':
+                item = User.objects.filter(Q(status=VALID)
+                                           & Q(user_no=user_no)).first();
+            else:
+                item = User.objects.filter(Q(status=VALID)
+                                           & Q(user_no=user_no)
+                                           & ~Q(id=user_id)).first();
+            if item is not None:
+                item_json = model2json(item);
+                res['data'] = item_json;
         except Exception as e:
             res["code"] = "500";
             res["err_msg"] = '获取用户失败';
@@ -126,8 +142,9 @@ def getList(request):
             search_user = json.loads(search_str);
             # 根据查询条件查询
             userList = User.objects.filter(
-                Q(user_no__contains=search_user["user_no"]) & Q(
-                    name__contains=search_user["name"])).order_by('-modify_date').values();
+                Q(status=VALID)
+                & Q(user_no__contains=search_user["user_no"])
+                & Q(name__contains=search_user["name"])).order_by('-modify_date').values();
             currentPage = int(search_user["currentPage"]);
             itemsPerPage = int(search_user["itemsPerPage"]);
             startIndex = (currentPage - 1) * itemsPerPage;
--- django_ajax/urls.py
+++ django_ajax/urls.py
@@ -24,11 +24,11 @@ urlpatterns = [
     # 用户
     url(r'^index/$', views.index, name="index"),  # 首页
     url(r'^add/$', views.add, name="add"),  # 添加
-    url(r'^edit/(\d+)$', views.edit, name="edit"),  # 修改
+    url(r'^edit/(\S{36})$', views.edit, name="edit"),  # 修改
     url(r'^getList/$', views.getList),  # 获取列表
     url(r'^getItem/$', views.getItem),  # 获取对象
-    url(r'^getItemByNo/$', views.getItemByNo),  # 根据用户编码获取对象
-    url(r'^delItem/(\d+)$', views.delItem),  # 删除对象
+    url(r'^isUserNOExists/$', views.isUserNOExists),  # 根据用户编码获取对象
+    url(r'^delItem/(\S{36})$', views.delItem),  # 删除对象
     url(r'^getPositionList/$', views.getPositionList),  # 获取岗位列表
     # 组织
     url(r'^orgindex/$', OrgView.index, name="orgindex"),  # 首页
